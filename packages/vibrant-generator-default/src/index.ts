e3bd7a23fb0adc94f0bd1f38cb8c15b133d8b904
Germán Méndez Bravo <german.mb@gmail.com>
2018-12-28 20:02:15 -0700

    Fill more empty swatches

--- packages/vibrant-generator-default/src/index.ts
+++ packages/vibrant-generator-default/src/index.ts
@@ -194,11 +194,35 @@ function _generateEmptySwatches(palette: Palette, maxPopulation: number, opts: D
         let [h, s, l] = palette.DarkVibrant.getHsl()
         l = opts.targetNormalLuma
         palette.Vibrant = new Swatch(hslToRgb(h, s, l), 0)
+    } else if (palette.Vibrant === null && palette.LightVibrant !== null) {
+        let [h, s, l] = palette.LightVibrant.getHsl()
+        l = opts.targetNormalLuma
+        palette.Vibrant = new Swatch(hslToRgb(h, s, l), 0)
     }
     if (palette.DarkVibrant === null && palette.Vibrant !== null) {
         let [h, s, l] = palette.Vibrant.getHsl()
         l = opts.targetDarkLuma
         palette.DarkVibrant = new Swatch(hslToRgb(h, s, l), 0)
+    }
+    if (palette.LightVibrant === null && palette.Vibrant !== null) {
+        let [h, s, l] = palette.Vibrant.getHsl()
+        l = opts.targetLightLuma
+        palette.LightVibrant = new Swatch(hslToRgb(h, s, l), 0)
+    }
+    if (palette.Muted === null && palette.Vibrant !== null) {
+        let [h, s, l] = palette.Vibrant.getHsl()
+        l = opts.targetMutesSaturation
+        palette.Muted = new Swatch(hslToRgb(h, s, l), 0)
+    }
+    if (palette.DarkMuted === null && palette.DarkVibrant !== null) {
+        let [h, s, l] = palette.DarkVibrant.getHsl()
+        l = opts.targetMutesSaturation
+        palette.DarkMuted = new Swatch(hslToRgb(h, s, l), 0)
+    }
+    if (palette.LightMuted === null && palette.LightVibrant !== null) {
+        let [h, s, l] = palette.LightVibrant.getHsl()
+        l = opts.targetMutesSaturation
+        palette.LightMuted = new Swatch(hslToRgb(h, s, l), 0)
     }
 }
 
