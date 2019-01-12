92d9942a12ceac98dcd4f64ce877c87e35967f86
AKFish <akfish@gmail.com>
2019-01-13 05:50:57 +0800

    Patch vibrant-color/src/index.ts

--- packages/vibrant-color/src/index.ts
+++ packages/vibrant-color/src/index.ts
@@ -1,23 +1,19 @@
-import { rgbToHsl, rgbToHex } from "./converter";
+import { rgbToHsl, rgbToHex } from './converter'
 
 export interface Filter {
-  (red: number, green: number, blue: number, alpha: number): boolean;
+  (red: number, green: number, blue: number, alpha: number): boolean
 }
 
-export interface Vec3 extends Array<number> {
-  0: number;
-  1: number;
-  2: number;
-}
+export type Vec3 = [number, number, number]
 
 export interface Palette {
-  Vibrant?: Swatch;
-  Muted?: Swatch;
-  DarkVibrant?: Swatch;
-  DarkMuted?: Swatch;
-  LightVibrant?: Swatch;
-  LightMuted?: Swatch;
-  [name: string]: Swatch | undefined;
+  Vibrant?: Swatch
+  Muted?: Swatch
+  DarkVibrant?: Swatch
+  DarkMuted?: Swatch
+  LightVibrant?: Swatch
+  LightMuted?: Swatch
+  [name: string]: Swatch | undefined
 }
 
 export class Swatch {
@@ -25,100 +21,107 @@ export class Swatch {
     return filters.length > 0
       ? colors.filter(({ r, g, b }) => {
           for (let j = 0; j < filters.length; j++) {
-            if (!filters[j](r, g, b, 255)) return false;
+            if (!filters[j](r, g, b, 255)) return false
           }
-          return true;
+          return true
         })
-      : colors;
+      : colors
   }
   static clone(swatch: Swatch) {
-    return new Swatch(swatch._rgb, swatch._population);
+    return new Swatch(swatch._rgb, swatch._population)
   }
-  private _hsl: Vec3;
-  private _rgb: Vec3;
-  private _yiq: number;
-  private _population: number;
-  private _hex: string;
+  private _hsl: Vec3
+  private _rgb: Vec3
+  private _yiq: number
+  private _population: number
+  private _hex: string
   get r(): number {
-    return this._rgb[0];
+    return this._rgb[0]
   }
   get g(): number {
-    return this._rgb[1];
+    return this._rgb[1]
   }
   get b(): number {
-    return this._rgb[2];
+    return this._rgb[2]
   }
   get rgb(): Vec3 {
-    return this._rgb;
+    return this._rgb
   }
   get hsl(): Vec3 {
     if (!this._hsl) {
-      let [r, g, b] = this._rgb;
-      this._hsl = rgbToHsl(r, g, b);
+      let [r, g, b] = this._rgb
+      this._hsl = rgbToHsl(r, g, b)
     }
-    return this._hsl;
+    return this._hsl
   }
   get hex() {
     if (!this._hex) {
-      let [r, g, b] = this._rgb;
-      this._hex = rgbToHex(r, g, b);
+      let [r, g, b] = this._rgb
+      this._hex = rgbToHex(r, g, b)
     }
-    return this._hex;
+    return this._hex
   }
   get population(): number {
-    return this._population;
+    return this._population
+  }
+  
+  toJSON() {
+    return {
+      rgb: this.rgb,
+      population: this.population
+    }
   }
 
   // TODO: deprecate internally, use property instead
   getRgb(): Vec3 {
-    return this._rgb;
+    return this._rgb
   }
   // TODO: deprecate internally, use property instead
   getHsl(): Vec3 {
-    return this.hsl;
+    return this.hsl
   }
   // TODO: deprecate internally, use property instead
   getPopulation(): number {
-    return this._population;
+    return this._population
   }
   // TODO: deprecate internally, use property instead
   getHex(): string {
-    return this.hex;
+    return this.hex
   }
 
   private getYiq(): number {
     if (!this._yiq) {
-      let rgb = this._rgb;
-      this._yiq = (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000;
+      let rgb = this._rgb
+      this._yiq = (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000
     }
-    return this._yiq;
+    return this._yiq
   }
 
-  private _titleTextColor: string;
-  private _bodyTextColor: string;
+  private _titleTextColor: string
+  private _bodyTextColor: string
 
   get titleTextColor() {
     if (this._titleTextColor) {
-      this._titleTextColor = this.getYiq() < 200 ? "#fff" : "#000";
+      this._titleTextColor = this.getYiq() < 200 ? '#fff' : '#000'
     }
-    return this._titleTextColor;
+    return this._titleTextColor
   }
   get bodyTextColor() {
     if (this._bodyTextColor) {
-      this._bodyTextColor = this.getYiq() < 150 ? "#fff" : "#000";
+      this._bodyTextColor = this.getYiq() < 150 ? '#fff' : '#000'
     }
-    return this._bodyTextColor;
+    return this._bodyTextColor
   }
   getTitleTextColor(): string {
-    return this.titleTextColor;
+    return this.titleTextColor
   }
 
   getBodyTextColor(): string {
-    return this.bodyTextColor;
+    return this.bodyTextColor
   }
 
   constructor(rgb: Vec3, population: number) {
-    this._rgb = rgb;
-    this._population = population;
+    this._rgb = rgb
+    this._population = population
   }
 }
