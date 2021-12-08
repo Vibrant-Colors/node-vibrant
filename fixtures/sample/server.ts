84b3f23040b4ede526e7ded3287c94345a72bad1
Corbin Crutchley <crutchcorn@gmail.com>
2021-12-08 00:13:35 -0800

    chore: fix build, update Lerna

--- fixtures/sample/server.ts
+++ fixtures/sample/server.ts
@@ -6,7 +6,11 @@ import serveStatic = require('serve-static')
 const staticFiles = serveStatic(path.join(__dirname, './images/'))
 const serverHandler = (req: http.IncomingMessage, res: http.ServerResponse) => {
   let done = finalhandler(req, res)
-  return staticFiles(req as any, res as any, done)
+  return staticFiles(req as any, res as any, () => {
+    done((err: unknown) => {
+      if (err) throw err
+    })
+  })
 }
 
 export const createSampleServer = () => http.createServer(serverHandler)
