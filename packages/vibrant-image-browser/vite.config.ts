4a4ba52b060749476ee27babeb0ed050f5f72068
Corbin Crutchley <git@crutchcorn.dev>
2024-12-15 06:28:14 -0800

    chore: migrate browser to vite

---
+++ packages/vibrant-image-browser/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, mergeConfig } from "vite";
+import { tanstackViteConfig } from "@tanstack/config/vite";
+
+const config = defineConfig({});
+
+export default mergeConfig(
+  config,
+  tanstackViteConfig({
+    entry: "./src/index.ts",
+    srcDir: "./src",
+  })
+);
