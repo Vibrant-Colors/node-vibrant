1a4adeb07ad84a8287aa786b501b12261b016cd0
Corbin Crutchley <git@crutchcorn.dev>
2024-12-26 06:20:30 -0800

    chore: fix prettier

--- examples/js/simple/src/main.ts
+++ examples/js/simple/src/main.ts
@@ -1,14 +1,15 @@
 import { Vibrant } from "node-vibrant/browser";
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <p>Loading...</p>
-`
+`;
 
-Vibrant.from("https://avatars.githubusercontent.com/Vibrant-Colors").getPalette()
-  .then(palette => {
-    app.innerHTML = `
+Vibrant.from("https://avatars.githubusercontent.com/Vibrant-Colors")
+	.getPalette()
+	.then((palette) => {
+		app.innerHTML = `
       <img src="https://avatars.githubusercontent.com/Vibrant-Colors" alt=""/>
       <ul>
         <li style="background-color: ${palette.Vibrant?.hex}; color: ${palette.Vibrant?.bodyTextColor};">Vibrant</li>
@@ -18,6 +19,5 @@ Vibrant.from("https://avatars.githubusercontent.com/Vibrant-Colors").getPalette(
         <li style="background-color: ${palette.LightMuted?.hex}; color: ${palette.LightMuted?.bodyTextColor};">Light Muted</li>
         <li style="background-color: ${palette.DarkMuted?.hex}; color: ${palette.DarkMuted?.bodyTextColor};">Dark Muted</li>
       </ul>
-    `
-  })
-
+    `;
+	});
--- website/src/page-components/index/Colors.tsx
+++ website/src/page-components/index/Colors.tsx
@@ -1,16 +1,16 @@
-import styles from './Colors.module.css'
-import VibrantColors from './VibrantColors';
+import styles from "./Colors.module.css";
+import VibrantColors from "./VibrantColors";
 
 interface ColorsProps {
-    file: string;
-    className?: string;
+	file: string;
+	className?: string;
 }
 
-export const Colors = ({file, className}: ColorsProps) => {
-    return (
-        <div className={className}>
-            <img className={styles.image} src={file} alt=""/>
-            <VibrantColors img={file}/>
-        </div>
-    )
-}
+export const Colors = ({ file, className }: ColorsProps) => {
+	return (
+		<div className={className}>
+			<img className={styles.image} src={file} alt="" />
+			<VibrantColors img={file} />
+		</div>
+	);
+};
